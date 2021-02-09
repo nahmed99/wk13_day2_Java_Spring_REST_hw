4d438761faa851a21ccc60e77124049d42a424b9
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-09 16:10:31 +0000

    initial commit

---
+++ src/main/java/com/example/codeclan/folderservice/FolderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.folderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/folderservice/FolderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.folderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
